e536dc70ca717a21cf19db8c9197b502ed5c6ad3
riverblo <riverblo.cy@gmail.com>
2021-04-22 19:10:17 +0900

    ちょっと修正

--- src/main/java/sample/uppercase/ToUpperCase.java
+++ src/main/java/sample/uppercase/ToUpperCase.java
@@ -1,17 +1,27 @@
 package sample.uppercase;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class ToUpperCase implements RequestHandler<PersonRequest, PersonResponse> {
+import java.util.HashMap;
+import java.util.Map;
+
+public class ToUpperCase implements RequestHandler<Map<String, String>, Map<String, String>> {
+
+  private static final Logger log = LoggerFactory.getLogger(ToUpperCase.class);
 
   @Override
-  public PersonResponse handleRequest(PersonRequest input, Context context) {
-
-    PersonResponse res = new PersonResponse();
-    res.setPerson_id(input.getPerson_id());
-    res.setName(input.getName().toUpperCase());
-    res.setMessage("Upper Success!!");
-    return res;
+  public Map<String, String> handleRequest(Map<String, String> input, Context context) {
+    LambdaLogger logger = context.getLogger();
+    log.error("log.errorrrrrrrrrr");
+    log.error("log.warn");
+    Map<String, String> response = new HashMap<>();
+    response.put("person_id", input.get("person_id"));
+    response.put("name", input.get("name").toUpperCase());
+    response.put("message", "Upper Success!!");
+    return response;
   }
 }
